6febf3fe421120abc267e17a05f989179378d59b
Noelle <noelle.maingi@student.moringaschool.com>
2023-07-24 13:48:54 +0300

    Updated the code

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,26 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Transactions from "./Transactions";
 import AddTransaction from "./AddTransaction";
-import SearchTransaction from "./SearchTransaction";
-import SortTransaction from "./SortTransaction";
 
 function App() {
   const [trans, setTrans] = useState([]);
-  const [searchResults, setSearchResult] = useState([]);
-  const [sortOption, setSortOption] = useState("category"); 
-  const [category, setCategory] = useState("All");
 
- 
-  useEffect(() => {
+  const fetchData=() =>{
     fetch("http://localhost:3000/transactions")
-    .then(json => {setTrans(json)
-    return json})
-    .then(json => {setSearchResult(json)})
-    },[])
+      .then(res =>{
+        if(!res.ok){
+          throw new Error('Failed')
+        }
+        return res.json()
+      })
+      .then((data) =>{
+        setTrans(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []);
 
   function AddNewTransaction(newTrans) {
     fetch("http://localhost:3000/transactions", {
@@ -39,6 +43,7 @@ function App() {
       });
   }
 
+   const [transact ,settransact] = useState([]);
   function handleDeleteTransaction(id) {
     fetch(`http://localhost:3000/transactions/${id}`, {
       method: "DELETE",
@@ -50,40 +55,23 @@ function App() {
         }
         throw new Error("Network response was not okay");
       })
-      .then(() => {
-        setTrans((transactions) =>
-          transactions.filter((transaction) => transaction.id !== id)
-        );
+      .then((data) => {
+        console.log(data)
+         const handledata = transact.filter((item)=> item.id !==id)
+         settransact(handledata)
+         fetchData()
       });
   }
-
-  // const filterTransactions = trans
-  //   .filter((transaction) => {
-  //     if (category === "All") {
-  //       return true;
-  //     } else {
-  //       return transaction.category === category;
-  //     }
-  //   })
-  //   .filter((transaction) =>
-  //     transaction.description.toLowerCase().includes(trans.toLowerCase())
-  //   );
-
-  const handleSortChange = (option) => {
-    setSortOption(option);
-  };
-
+ 
+ 
 
   return (
     <div>
       <div>
         <h1> BANK OF FLATIRON </h1>
       </div>
-      <SortTransaction sortValue={handleSortChange} />
-      <Transactions
-        // sortedTransactions={sortedTransactions} 
-        handleDeleteTransaction={handleDeleteTransaction}
-      />
+
+      <Transactions handleDeleteTransaction={handleDeleteTransaction} />
       <AddTransaction onFormSubmit={AddNewTransaction} />
     </div>
   );
